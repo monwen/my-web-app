e2bc310fdb2683dddac96ced6bd50b26c1a4c393
Monwen <mshen0505@gmail.com>
2023-08-20 17:23:00 -0700

    adding about me

--- src/components/about.jsx
+++ src/components/about.jsx
@@ -5,7 +5,12 @@ class About extends Component {
     render() { 
         return (
             <div className='content'>
-                <h1> This is about page</h1>
+                <h1> Hey There, I'm Monwen!</h1>
+                <p>
+                Welcome to my corner of the digital universe.
+                I'm the tech enthusiast and coding aficionado behind the screen, and I'm thrilled to give you a sneak peek into my journey as a software developer.
+                With a cup of Java (both the programming language and the drink) in hand, I've been crafting digital wonders and diving into the exciting world of technology.
+                </p>
             </div>
         );
     }
